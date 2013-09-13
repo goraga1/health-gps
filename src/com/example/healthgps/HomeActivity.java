72343ab0ec833a704a5d6c73de09901f001843e5
goraga1 <vahan@Vahans-MacBookPro.local>
2013-09-13 18:26:53 +0400

    mm

--- src/com/example/healthgps/HomeActivity.java
+++ src/com/example/healthgps/HomeActivity.java
@@ -1,19 +1,23 @@
 package com.example.healthgps;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.ImageButton;
 
-public class HomeActivity  extends Activity{
-	
-	
-	
+public class HomeActivity extends Activity {
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
 		setContentView(R.layout.activity_home);
-<<<<<<< HEAD
 
 		ImageButton hospitalButton = (ImageButton) findViewById(R.id.hospital);
 		hospitalButton.setOnClickListener(new OnClickListener() {
@@ -21,16 +25,14 @@ public class HomeActivity  extends Activity{
 			@TargetApi(Build.VERSION_CODES.JELLY_BEAN)
 			@SuppressLint("NewApi")
 			public void onClick(View button) {
-				
-				Intent hospitalList = new Intent(HomeActivity.this, TabLayoutActivity.class);
-				
-				
+
+				Intent hospitalList = new Intent(HomeActivity.this,
+						TabLayoutActivity.class);
+
 				startActivity(hospitalList);
-				overridePendingTransition(R.anim.animation,
-						R.anim.animation1);
-			//	startActivity(hospitalList);
-				
-				
+				overridePendingTransition(R.anim.animation, R.anim.animation1);
+				// startActivity(hospitalList);
+
 				if (button.isSelected()) {
 					button.setSelected(false);
 					// ...Handle toggle off
@@ -42,15 +44,12 @@ public class HomeActivity  extends Activity{
 
 		});
 
-=======
->>>>>>> 508ba387ad5f880db527ee9b8d9a6c17a7bdd720
 	}
 
-	
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.main, menu);
-//		return true;
-//	}
+	// public boolean onCreateOptionsMenu(Menu menu) {
+	// // Inflate the menu; this adds items to the action bar if it is present.
+	// getMenuInflater().inflate(R.menu.main, menu);
+	// return true;
+	// }
 
 }
